cb980937814805e58ce4828a8a8f4e3805a062a6
Sumit Kumar Sahu <sumitsahu89845@gmail.com>
2025-06-28 18:56:38 +0530

    Add files via upload

---
+++ week 2/CustomerCommTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mailSenderMock;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _mailSenderMock = new Mock<IMailSender>();
+
+            _mailSenderMock
+                .Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            _customerComm = new CustomerCommLib.CustomerComm(_mailSenderMock.Object);
+        }
+
+        [Test]
+        [TestCase]
+        public void SendMailToCustomer_WhenCalled_ReturnsTrue()
+        {
+            var result = _customerComm.SendMailToCustomer();
+            Assert.That(result, Is.True);
+        }
+    }
+}
